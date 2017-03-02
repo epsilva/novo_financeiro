e87a362970d8cfabdc4f41722f9c2bc20af9f7de
epsilva <eps.94@hotmail.com>
2017-03-02 19:34:01 -0300

    primeiro commit

---
+++ src/main/java/br/com/devipi/bibliotecadigital/NovoFinanceiroApplication.java
@@ -0,0 +1,12 @@
+package br.com.devipi.bibliotecadigital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NovoFinanceiroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NovoFinanceiroApplication.class, args);
+	}
+}
